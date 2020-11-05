a7a5392da7a4b345c434716bb8720ce148ccb681
sioncojp <sion_cojp@yahoo.co.jp>
2020-11-06 00:21:51 +0900

    debugging mode for delve

--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/sioncojp/terraform-provider-jamf/jamf"
+	"log"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: jamf.Provider,
-	})
+	var debugMode bool
+
+	flag.BoolVar(&debugMode, "debuggable", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	if debugMode {
+		err := plugin.Debug(context.Background(), "registry.terraform.io/sioncojp/jamf",
+			&plugin.ServeOpts{
+				ProviderFunc: jamf.Provider,
+			})
+		if err != nil {
+			log.Println(err.Error())
+		}
+	} else {
+		plugin.Serve(&plugin.ServeOpts{
+			ProviderFunc: jamf.Provider,
+		})
+	}
+
 }
