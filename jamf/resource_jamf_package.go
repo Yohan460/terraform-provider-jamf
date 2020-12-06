831cf3708f4d44e32fd65c49d1fde0363d889837
Johan McGwire <johan.mcgwire@coinbase.com>
2020-12-06 17:51:31 -0500

    Init Package support

---
+++ jamf/resource_jamf_package.go
@@ -0,0 +1,10 @@
+package jamf
+
+// Jamf does not have official documentation on how to upload an actual package file
+// via the API. Therefore I'm not including this by default. If anyone wants to submit
+// a PR to enable this "aftermarket" functionality based on JSSImporter or other third
+// party packages, then please do.
+
+// Plz help Jamf, you're our only hope
+
+// -Yohan
