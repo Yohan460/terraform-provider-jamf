c9a72e06f8467abaaca020c2c5a9305d7f8322ae
Harry Seeber <harry.seeber@faire.com>
2024-03-21 17:48:17 -0600

    update to latest go-jamf-api dep and terraform 1.6.5

--- jamf/provider_test.go
+++ jamf/provider_test.go
@@ -18,54 +18,43 @@ var providerFactories = map[string]func() (*schema.Provider, error){
 // such as environment variables containing test keys that are used to
 // configure the Provider or Resource under test.
 func testAccPreCheck(t *testing.T) {
-	if !isUsernameSet() {
-		t.Fatal("JAMF_USERNAME environment variable must be set for acceptance tests")
+	if isClientIdSet() {
+		if !isClientSecretSet() {
+			t.Fatal("JAMF_CLIENT_SECRET environment variable must be set for acceptance tests (if JAMF_CLIENT_ID is set")
+		}
+	} else if isUsernameSet() {
+		if !isPasswordSet() {
+			t.Fatal("JAMF_PASSWORD environment variable must be set for acceptance tests (if JAMF_USERNAME is set)")
+		}
+	} else {
+		t.Fatal("JAMF_USERNAME or JAMF_CLIENT_ID environment variable must be set for acceptance tests")
 	}
-	if !isPasswordSet() {
-		t.Fatal("JAMF_PASSWORD environment variable must be set for acceptance tests")
-	}
+
 	if !isURLSet() {
 		t.Fatal("JAMF_URL environment variable must be set for acceptance tests")
 	}
-	if !isClientIdSet() {
-		t.Fatal("JAMF_CLIENT_ID environment variable must be set for acceptance tests")
-	}
-	if !isClientSecretSet() {
-		t.Fatal("JAMF_CLIENT_SECRET environment variable must be set for acceptance tests")
-	}
+}
+
+func isVarSet(variable string) bool {
+	return os.Getenv(variable) != ""
 }
 
 func isUsernameSet() bool {
-	if os.Getenv("JAMF_USERNAME") != "" {
-		return true
-	}
-	return false
+	return isVarSet("JAMF_USERNAME")
 }
 
 func isPasswordSet() bool {
-	if os.Getenv("JAMF_PASSWORD") != "" {
-		return true
-	}
-	return false
+	return isVarSet("JAMF_PASSWORD")
 }
 
 func isURLSet() bool {
-	if os.Getenv("JAMF_URL") != "" {
-		return true
-	}
-	return false
+	return isVarSet("JAMF_URL")
 }
 
 func isClientIdSet() bool {
-	if os.Getenv("JAMF_CLIENT_ID") != "" {
-		return true
-	}
-	return false
+	return isVarSet("JAMF_CLIENT_ID")
 }
 
 func isClientSecretSet() bool {
-	if os.Getenv("JAMF_CLIENT_SECRET") != "" {
-		return true
-	}
-	return false
+	return isVarSet("JAMF_CLIENT_SECRET")
 }
